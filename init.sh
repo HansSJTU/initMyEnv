3ff6b788305e11adf3446887c07841f880ba2f7a
HansSJTU <Hanshosjtu@gmail.com>
2017-08-29 15:48:45 +0800

    modify init

--- init.sh
+++ init.sh
@@ -5,20 +5,19 @@
 #
 # Distributed under terms of the MIT license.
 #
+work_dir=$(pwd)
+log_dir="${work_dir}/Logs/"
 
-log_dir="./Logs/"
-
-if [ ! -d ${log_dir} ]; then
-    mkdir -p ${log_dir}
+if [ ! -d "${log_dir}" ]; then
+    mkdir -p "${log_dir}"
 fi
 
-log_dir_abs=`realpath ${log_dir}`
-current_time=`date +%Y%m%d`_`date +%T`
+current_time="$(date +%Y%m%d)_$(date +%T)"
 log_name="initLog_${current_time}"
 
 pushd ./other_install > /dev/null
-rm -rf ${log_dir_abs}/{info,err}
-ln -s ${log_dir_abs}/${log_name}.err ${log_dir_abs}/err
-ln -s ${log_dir_abs}/${log_name}.info ${log_dir_abs}/info
-./init_mac_or_linux.sh $@ 2> >(tee "${log_dir_abs}/${log_name}.err") 1> >(tee "${log_dir_abs}/${log_name}.info")
+rm -rf "${log_dir}"/{info,err}
+ln -s "${log_dir}/${log_name}.err" "${log_dir}/err"
+ln -s "${log_dir}/${log_name}.info" "${log_dir}/info"
+./init_mac_or_linux.sh "$@" 2> >(tee "${log_dir}/${log_name}.err") 1> >(tee "${log_dir}/${log_name}.info")
 popd > /dev/null
