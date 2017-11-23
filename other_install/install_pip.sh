bc45e9d0e82050401193dd7b6ca2ecfb8494ef1d
HansSJTU <Hanshosjtu@gmail.com>
2017-11-23 00:17:52 +0000

    tensorflow_init

---
+++ other_install/install_pip.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+#
+# install_pip.sh
+# Copyright (C) 2017-11-23 Hanxiao <hah114@ucsd.edu>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+sudo apt-get install python-pip python-dev build-essential
+sudo pip install --upgrade pip
+sudo pip install --upgrade virtualenv
---
+++ other_install/install_tensorflow.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#
+# install_tensorflow.sh
+# Copyright (C) 2017-11-23 Hanxiao <hah114@ucsd.edu>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+
