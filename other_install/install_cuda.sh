095a8fdc495b1328a87b41467227cb3db5c52fa4
HansSJTU <Hanshosjtu@gmail.com>
2018-02-07 21:23:13 -0800

    modify install cuda and tensorflow

--- other_install/install_cuda.sh
+++ other_install/install_cuda.sh
@@ -6,29 +6,56 @@
 # Distributed under terms of the MIT license.
 #
 
-echo -e "\n****** [INFO] *******\nThis script is to install CUDA 8.0 with CUBLAS 8.0 and CUDNN 6.0"
-echo "The CUDA version dose not support Tensorflow 1.5!"
-sleep 3
-echo "Downloading will start in 5 seconds"
-sleep 5
+echo -e "\n******* [INFO] *******\nThis script is to install: \n \
+    \t(1) CUDA 8.0 with CUBLAS 8.0 and CUDNN 6.0 (for tensorflow 1.0) on Ubuntu 14.04 \n \
+    \t(2) CUDA 9.0 with CUBLAS 9.0 and CUDNN 7.0 (for tensorflow 1.5) on Ubuntu 16.04"
+sleep 1
+read -rp "Version To Install (1/2): " version
+
+if [[ ${version} -eq 1 ]]; then
+    cuda_addr="http://108.61.247.112/public/cuda-repo-ubuntu1404-8-0-local-ga2_8.0.61-1_amd64.deb"
+    cublas_addr="http://108.61.247.112/public/cuda-repo-ubuntu1404-8-0-local-cublas-performance-update_8.0.61-1_amd64.deb"
+    cudnn_addr="http://108.61.247.112/public/libcudnn6_6.0.21-1+cuda8.0_amd64.deb"
+    suffix="8.0"
+elif [[ ${version} -eq 2 ]]; then
+    cuda_addr="http://108.61.247.112/public/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64.deb"
+    cublas_addr="http://108.61.247.112/public/cuda-repo-ubuntu1604-9-0-local-cublas-performance-update_1.0-1_amd64.deb"
+    cudnn_addr="http://108.61.247.112/public/libcudnn7_7.0.5.15-1+cuda9.0_amd64.deb"
+    suffix="9.0"
+else
+    echo "[ERROR] Version Entered Error, exiting..." 1>&2
+    exit -1
+fi
 
 if [[ ! -d ~/cuda_install ]]; then
     mkdir -p ~/cuda_install
 fi
 
+# download all files
 pushd ~/cuda_install > /dev/null
-wget "http://108.61.247.112/public/cuda-repo-ubuntu1404-8-0-local-ga2_8.0.61-1_amd64.deb"
-wget "http://108.61.247.112/public/cuda-repo-ubuntu1404-8-0-local-cublas-performance-update_8.0.61-1_amd64.deb"
-wget "http://108.61.247.112/public/libcudnn6_6.0.21-1+cuda8.0_amd64.deb"
+wget -O cuda_${suffix}.deb "${cuda_addr}"
+wget -O cublas_${suffix}.deb "${cublas_addr}"
+wget -O cudnn_${suffix}.deb "${cudnn_addr}"
 
-sudo dpkg -i "cuda-repo-ubuntu1404-8-0-local-ga2_8.0.61-1_amd64.deb"
+# install CUDA
+sudo dpkg -i cuda_${suffix}.deb
 sudo apt-get update
 sudo apt-get install cuda -y
-sudo dpkg -i "cuda-repo-ubuntu1404-8-0-local-cublas-performance-update_8.0.61-1_amd64.deb"
-sudo dpkg -i "libcudnn6_6.0.21-1+cuda8.0_amd64.deb"
+
+# install cuBLAS and cuDNN
+sudo dpkg -i cublas_${suffix}.deb
+sudo dpkg -i cudnn_${suffix}.deb
 sudo apt-get install libcupti-dev -y
-sudo apt-get install linux-headers-$(uname -r) -y
+sudo apt-get install "linux-headers-$(uname -r)" -y
 popd > /dev/null
 
-echo "export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/:/usr/local/cuda/lib64:\$LD_LIBRARY_PATH" >> ~/.bashrc
-echo "export PATH=/usr/local/cuda-8.0/bin:/usr/local/cuda/bin:\$PATH" >> ~/.bashrc
+bashrc_inject_1="export LD_LIBRARY_PATH=/usr/local/cuda-${suffix}/lib64/:/usr/local/cuda/lib64:\$LD_LIBRARY_PATH"
+bashrc_inject_2="export PATH=/usr/local/cuda-${suffix}/bin:/usr/local/cuda/bin:\$PATH"
+if [[ $(grep -c "${bashrc_inject_1}" "${HOME}"/.bashrc) -eq 0 ]]; then
+    echo "${bashrc_inject_1}" >> ~/.bashrc
+fi
+if [[ $(grep -c "${bashrc_inject_2}" "${HOME}"/.bashrc) -eq 0 ]]; then
+    echo "${bashrc_inject_2}" >> ~/.bashrc
+fi
+
+echo "You can clean the install package if the installment is finished: ${HOME}/cuda_install"
--- other_install/install_tensorflow_1.0.sh
+++ other_install/install_tensorflow_1.0.sh
@@ -1,16 +1,71 @@
 #! /bin/bash
 #
-# install_tensorflow_1.0.sh
+# install_tensorflow.sh
 # Copyright (C) 2017-11-23 Hanxiao <hah114@ucsd.edu>
 #
 # Distributed under terms of the MIT license.
 #
 
-command -v pip >/dev/null 2>&1 || { ./install_pip.sh; }
+echo -e "\n******* [INFO] *******\nThis script is to install different versions of tensorflow: \n"
+sleep 1
 
-if [ "$(uname)" == "Darwin" ]; then
-    sudo pip install --upgrade tensorflow-gpu https://pypi.python.org/packages/0d/d7/b49a6ceb055f392f91bce25eb6e1665f9b2f0a4628f7acdbccf1cd1d0ee6/tensorflow-1.0.0-cp27-cp27m-macosx_10_11_x86_64.whl#md5=51235fbe34e705bcdb1b5bbd6a482c57
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    sudo pip install --upgrade tensorflow-gpu https://pypi.python.org/packages/7b/c5/a97ed48fcc878e36bb05a3ea700c077360853c0994473a8f6b0ab4c2ddd2/tensorflow-1.0.0-cp27-cp27mu-manylinux1_x86_64.whl#md5=a7483a4da4d70cc628e9e207238f77c0
+read -rp "Version to Install (1.0.0, 1.5.0, 1.6.0): " version
+if [[ $(echo "${version}" | grep -c "[1].[0-9].[0-9]") -eq 0 ]]; then
+    echo "[ERROR] Version number error"
+    exit -1
 fi
 
+read -rp "Python Version (2.7, 3.4, 3.5, 3.6): " python_version
+if [[ $(echo "${python_version}" | grep -c "[23].[4567]") -eq 0 ]]; then
+    echo "[ERROR] Python version number error"
+    exit -1
+fi
+
+read -rp "GPU Enable (1 for GPU, 0 for CPU): " GPU_enable
+if [[ $(echo "${GPU_enable}" | grep -c "[01]") -eq 0 ]]; then
+    echo "[ERROR] GPU Enable number error"
+    exit -1
+fi
+
+function validate_url
+{
+    if [[ $(wget -S --spider "$1" 2>&1 | grep 'HTTP/1.1 200 OK') ]]; then
+        echo "true"
+    else
+        echo "false"
+    fi
+}
+
+py_version_1=${python_version:0:1}
+py_version_2=${python_version:2:3}
+if [ "$(uname)" == "Darwin" ]; then
+    if [[ ${GPU_enable} -eq 1 ]]; then
+        echo "[ERROR] Mac does not support gpu version, exiting..." 1>&2
+        exit -1
+    fi
+    url="https://storage.googleapis.com/tensorflow/mac/cpu/tensorflow-${version}-py${py_version_1}-none-any.whl"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    if [[ ${GPU_enable} -eq 1 ]]; then
+        proc="gpu"
+    else
+        proc="cpu"
+    fi
+    if [[ ${py_version_1} -eq 2 ]]; then
+        url="https://storage.googleapis.com/tensorflow/linux/${proc}/tensorflow-${version}-cp${py_version_1}${py_version_2}-none-linux_x86_64.whl"
+    else
+        url="https://storage.googleapis.com/tensorflow/linux/${proc}/tensorflow-${version}-cp${py_version_1}${py_version_2}-cp${py_version_1}${py_version_2}m-linux_x86_64.whl"
+    fi
+fi
+echo ${url}
+
+if [[ $(validate_url "${url}") == 'false' ]]; then
+    echo "[ERROR] File dose not exists: ${url}"
+    exit -1
+fi
+
+if [[ ${py_version_1} -eq 2 ]]; then
+    command -v pip >/dev/null 2>&1 || { ./install_pip.sh; }
+    sudo pip install --upgrade ${url}
+else
+    sudo pip3 install --upgrade ${url}
+fi
