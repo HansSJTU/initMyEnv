fae05d534cd21ac1cb175971bb4234cb389f2568
HansSJTU <Hanshosjtu@gmail.com>
2018-04-06 22:07:44 +0000

    tmp

--- other_install/install_cuda.sh
+++ other_install/install_cuda.sh
@@ -7,21 +7,27 @@
 #
 
 echo -e "\n******* [INFO] *******\nThis script is to install: \n \
-    \t(1) CUDA 8.0 with CUBLAS 8.0 and CUDNN 6.0 (for tensorflow 1.0) on Ubuntu 14.04 \n \
-    \t(2) CUDA 9.0 with CUBLAS 9.0 and CUDNN 7.0 (for tensorflow 1.5) on Ubuntu 16.04"
+    \t(1) CUDA 8.0 with CUBLAS Patch and CUDNN 6.0 (for tensorflow 1.0) on Ubuntu 14.04 \n \
+    \t(2) CUDA 9.0 with CUBLAS 9.0 and CUDNN 7.0 (for tensorflow 1.5) on Ubuntu 16.04 \n \
+    \t(3) CUDA 9.1 with CUBLAS 9.0 and CUDNN 7.1.2\n"
 sleep 1
-read -rp "Version To Install (1/2): " version
+read -rp "Version To Install (1/2/3): " version
 
 if [[ ${version} -eq 1 ]]; then
-    cuda_addr="http://108.61.247.112/public/cuda-repo-ubuntu1404-8-0-local-ga2_8.0.61-1_amd64.deb"
+    cuda_addr="https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda-repo-ubuntu1404-8-0-local-ga2_8.0.61-1_amd64-deb"
     cublas_addr="http://108.61.247.112/public/cuda-repo-ubuntu1404-8-0-local-cublas-performance-update_8.0.61-1_amd64.deb"
     cudnn_addr="http://108.61.247.112/public/libcudnn6_6.0.21-1+cuda8.0_amd64.deb"
     suffix="8.0"
 elif [[ ${version} -eq 2 ]]; then
-    cuda_addr="http://108.61.247.112/public/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64.deb"
+    cuda_addr="https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb"
     cublas_addr="http://108.61.247.112/public/cuda-repo-ubuntu1604-9-0-local-cublas-performance-update_1.0-1_amd64.deb"
     cudnn_addr="http://108.61.247.112/public/libcudnn7_7.0.5.15-1+cuda9.0_amd64.deb"
     suffix="9.0"
+elif [[ ${version} -eq 3 ]]; then
+    cuda_addr="https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64"
+    cublas_addr="https://developer.nvidia.com/compute/cuda/9.1/Prod/patches/3/cuda-repo-ubuntu1604-9-1-local-cublas-performance-update-3_1.0-1_amd64"
+    cudnn_addr="108.61.247.112/public/libcudnn7_7.1.2.21-1+cuda9.1_amd64.deb"
+    suffix="9.1"
 else
     echo "[ERROR] Version Entered Error, exiting..." 1>&2
     exit -1
@@ -40,13 +46,13 @@ wget -O cudnn_${suffix}.deb "${cudnn_addr}"
 # install CUDA
 sudo dpkg -i cuda_${suffix}.deb
 sudo apt-get update
-sudo apt-get install cuda -y
+sudo apt-get install cuda
 
 # install cuBLAS and cuDNN
 sudo dpkg -i cublas_${suffix}.deb
 sudo dpkg -i cudnn_${suffix}.deb
-sudo apt-get install libcupti-dev -y
-sudo apt-get install "linux-headers-$(uname -r)" -y
+sudo apt-get install libcupti-dev
+sudo apt-get install "linux-headers-$(uname -r)"
 popd > /dev/null
 
 bashrc_inject_1="export LD_LIBRARY_PATH=/usr/local/cuda-${suffix}/lib64/:/usr/local/cuda/lib64:\$LD_LIBRARY_PATH"
