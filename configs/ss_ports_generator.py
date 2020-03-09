1c1697e1e86ec7d3ae9f1b6a958b71a918dca235
HansSJTU <Hanshosjtu@gmail.com>
2020-03-08 19:07:00 -0700

    sync change

--- configs/ss_ports_generator.py
+++ configs/ss_ports_generator.py
@@ -7,12 +7,77 @@
 # Distributed under terms of the MIT license.
 
 from tabulate import tabulate
+import os
 
-contents = []
+import PIL
+import PIL.Image
+import PIL.ImageFont
+import PIL.ImageOps
+import PIL.ImageDraw
 
-with open('./ss_ports', 'r') as f:
-    for line in f.readlines():
-        contents.append(line.strip().split('|'))
+PIXEL_ON = 0  # PIL color to use for "on"
+PIXEL_OFF = 255  # PIL color to use for "off"
 
-with open('./ss_ports_table', 'w') as f:
-    f.write(tabulate(contents[1:], contents[0], tablefmt="grid"))
+
+def main():
+    contents = []
+    with open('./ss_ports', 'r') as f:
+        for line in f.readlines():
+            contents.append(line.strip().split('|'))
+
+    with open('./ss_ports_table', 'w') as f:
+        f.write(tabulate(contents[1:], contents[0], tablefmt="grid"))
+    image = text_image('ss_ports_table', '/Library/Fonts/Menlo.ttc')
+    image.save('ss_ports_table.jpg')
+    os.system('rm ./ss_ports_table')
+
+def text_image(text_path, font_path=None):
+    """Convert text file to a grayscale image with black characters on a white background.
+
+    arguments:
+    text_path - the content of this file will be converted to an image
+    font_path - path to a font file (for example impact.ttf)
+    """
+    grayscale = 'L'
+    # parse the file into lines
+    with open(text_path) as text_file:  # can throw FileNotFoundError
+        lines = tuple(l.rstrip() for l in text_file.readlines())
+
+    # choose a font (you can see more detail in my library on github)
+    large_font = 20  # get better resolution with larger size
+    font_path = font_path or 'cour.ttf'  # Courier New. works in windows. linux may need more explicit path
+    try:
+        font = PIL.ImageFont.truetype(font_path, size=large_font)
+    except IOError:
+        font = PIL.ImageFont.load_default()
+        print('Could not use chosen font. Using default.')
+
+    # make the background image based on the combination of font and lines
+    pt2px = lambda pt: int(round(pt * 96.0 / 72))  # convert points to pixels
+    max_width_line = max(lines, key=lambda s: font.getsize(s)[0])
+    # max height is adjusted down because it's too large visually for spacing
+    test_string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    max_height = pt2px(font.getsize(test_string)[1])
+    max_width = pt2px(font.getsize(max_width_line)[0])
+    height = max_height * len(lines)  # perfect or a little oversized
+    width = int(round(max_width + 40))  # a little oversized
+    image = PIL.Image.new(grayscale, (width, height), color=PIXEL_OFF)
+    draw = PIL.ImageDraw.Draw(image)
+
+    # draw each line of text
+    vertical_position = 5
+    horizontal_position = 5
+    line_spacing = int(round(max_height * 0.8))  # reduced spacing seems better
+    for line in lines:
+        draw.text((horizontal_position, vertical_position),
+                  line, fill=PIXEL_ON, font=font)
+        vertical_position += line_spacing
+    # crop the text
+    c_box = PIL.ImageOps.invert(image).getbbox()
+    image = image.crop(c_box)
+    return image
+
+
+if __name__ == '__main__':
+    main()
+
